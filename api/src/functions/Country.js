660484699936ba5c811a16bef800b1b2de6ef8bc
anayaricardo <einstein.letsgetrelative@gmail.com>
2022-05-10 07:55:27 -0500

    css

--- api/src/functions/Country.js
+++ api/src/functions/Country.js
@@ -4,8 +4,8 @@ const sequelize = require("sequelize");
 
 module.exports = {
   getCountries: async (req, res) => {
-    const countriesApi = await axios.get("https://restcountries.com/v3.1/all");
     try {
+    const countriesApi = await axios.get("https://restcountries.com/v3.1/all");
       if (req.query.name) {
         return await Country.findAll({
           include: [
@@ -17,13 +17,13 @@ module.exports = {
             name: { [sequelize.Op.like]: `%${req.query.name}%` },
           },
         })
-          .then((countries) =>
-            countries.length
-              ? res.send(200)(countries)
-              : res.status(404).send({ message: "No countries found" })
+          .then((country) =>
+            country.length
+              ? res.send(country)
+              : res.status(404).send("No countries found")
           )
           .catch((err) =>
-            res.status(500).send({ message: "Internal server error" })
+            {error}
           );
       }
       await Promise.all(
@@ -41,11 +41,11 @@ module.exports = {
           let data = {
             id: cca3,
             name: name.common,
-            flags: flags.png,
+            flags: flags.svg,
             // flags: flags.svg, //* probar con svg
             continents:
-              continents[0].split(", ").length > 1
-                ? continents[0].split(", ")[1]
+              continents[0].split(" ").length > 1
+                ? continents[0].split(" ")[1]
                 : continents[0], // Ternario para que no de error si no tiene continente y para separar los continentes
             capital: capital ? capital[0] : "The country has no capital", // Ternario para que no de error si no tiene capital
             subregion: subregion ? subregion : "The country has no subregion", // Ternario para que no de error si no tiene subregion
@@ -64,48 +64,94 @@ module.exports = {
         limit: 250,
       }).then((results) => res.send(results));
     } catch (error) {
-      res.status(500).send({ message: "Internal server error" });
+      console.error(error)
     }
   },
 
-  getCountry: async (req, res) => {
-    if (req.query.name) {
-      return await Country.findAll({
+  getByName: async (req,res) => {
+    try {
+      // const name = req.params.name;
+      const country = await Country.findAll({
+        include: [
+          {
+            model: Activity,
+          }
+        ],
+        where: {
+          name: { [sequelize.Op.iLike]: `%${req.params.name}%` },
+        },
+      });
+      if (country) {
+        res.send(country);
+      } else {
+        res.status(404).send("Country not found");
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  // getByName: async (req, res) => {
+  //   if (req.query.name) {
+  //     return await Country.findAll({
+  //       include: [
+  //         {
+  //           model: Activity,
+  //         },
+  //       ],
+  //       where: { name: { [sequelize.Op.iLike]: `%${req.query.name}%` } },
+  //     })
+  //       .then((country) =>
+  //         country.length
+  //           ? res.send(country)
+  //           : res.status(404).send("Country not found")
+  //       )
+  //       .catch((error) => {
+  //         error;
+  //       });
+  //   }
+  // },
+
+  getById: async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const country = await Country.findAll({
         include: [
           {
             model: Activity,
           },
         ],
-        where: { name: { [sequelize.Op.iLike]: `%${req.query.name}%` } },
-      })
-        .then((country) =>
-          country.length
-            ? res.send(country)
-            : res.status(404).send("Country not found")
-        )
-        .catch((error) => {
-          error;
-        });
+        where: {
+          id: { [sequelize.Op.iLike]: `${id}` },
+        },
+      });
+      if (country) {
+        res.send(country);
+      } else {
+        res.status(404).send("Country not found");
+      }
+    } catch (error) {
+      console.error(error)
     }
   },
 
-  getId: async (req, res, next) => {
-    const id = req.params.id;
-    return await Country.findAll({
-      include: [
-        {
-          model: Activity,
-        },
-      ],
-      where: { id: { [sequelize.Op.iLike]: `${id}` } },
-    })
-      .then((results) =>
-        results.length
-          ? res.send(results)
-          : res.status(404).send("ID not found")
-      )
-      .catch((error) => next(error));
-  },
+  // getById: async (req, res, next) => {
+  //   const id = req.params.id;
+  //   return await Country.findAll({
+  //     include: [
+  //       {
+  //         model: Activity,
+  //       },
+  //     ],
+  //     where: { id: { [sequelize.Op.iLike]: `${id}` } },
+  //   })
+  //     .then((results) =>
+  //       results.length
+  //         ? res.send(results)
+  //         : res.status(404).send("ID not found")
+  //     )
+  //     .catch((error) => next(error));
+  // },
 
   getAll: async (req, res) => {
     if (req.query.name) {
--- api/src/routes/Country.js
+++ api/src/routes/Country.js
@@ -3,7 +3,8 @@ const router = Router();
 const functions = require("../functions/Country");
 
 router.get("/", functions.getCountries);
-router.get("/:id", functions.getId);
 router.get("/getAll", functions.getAll);
+router.get("/:id", functions.getById);
+// router.get("/:name", functions.getByName);
 
 module.exports = router;
